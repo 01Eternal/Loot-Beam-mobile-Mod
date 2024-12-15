584742cfbb75703fd5acac0bb39341ae44fb911f
TLEternal <109238149+01Eternal@users.noreply.github.com>
2024-12-15 03:02:45 -0300

    Added Main Documentation

--- TL/Modified/1.mod/main.js
+++ TL/Modified/1.mod/main.js
@@ -12,6 +12,15 @@ const Draw =
     ];
 const Color_Op_Multiply = Color["Color op_Multiply(Color a, float amount)"];
 
+/**
+ * Draw a texture with specified parameters.
+ * @param {Texture2D} texture - The texture to draw.
+ * @param {Vector2} position - The position on the screen where the texture will be drawn.
+ * @param {Color} color - The color to apply to the texture.
+ * @param {number} rotation - The rotation angle of the texture.
+ * @param {Vector2} origin - The origin point for rotation and scaling.
+ * @param {number} scale - The scale factor of the texture.
+ */
 function drawTexture(texture, position, color, rotation, origin, scale) {
     Main.spriteBatch[
         "void Draw(Texture2D texture, Vector2 position, Nullable`1 sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)"
@@ -26,7 +35,6 @@ let SimpleGlow = "";
 
 Main.Initialize_AlmostEverything.hook((original, self) => {
     original(self);
-
     SimpleBeam = new ModTexture(`SimpleBeam`).asset.asset.Value;
     Center = new ModTexture(`Center`).asset.asset.Value;
     SimpleGlow = new ModTexture(`SimpleGlow`).asset.asset.Value;
@@ -48,41 +56,36 @@ function rectangle(x, y, width, height) {
     return rectangle;
 }
 
+/**
+ * Get the color corresponding to the item's rarity.
+ * @param {number} rare - The rarity ID of the item.
+ * @returns {Color} - The corresponding rarity color.
+ */
 function getRarityColor(rare) {
-    switch (rare) {
-        case -1:
-            return Color.Gray;
-        case 0:
-            return Color.White;
-        case 1:
-            return Color.Blue;
-        case 2:
-            return Color.Green;
-        case 3:
-            return Color.Orange;
-        case 4:
-            return Color.Red;
-        case 5:
-            return Colors.RarityPink;
-        case 6:
-            return Color.LightPurple;
-        case 7:
-            return Color.Lime;
-        case 8:
-            return Color.Yellow;
-        case 9:
-            return Color.Cyan;
-        case 10:
-            return Color.Red;
-        case 11:
-            return Color.Purple;
-        default:
-            return Color.White;
-    }
+    const rarityColors = {
+        [-1]: Color.Gray,
+        0: Color.White,
+        1: Color.Blue,
+        2: Color.Green,
+        3: Color.Orange,
+        4: Color.Red,
+        5: Colors.RarityPink,
+        6: Color.LightPurple,
+        7: Color.Lime,
+        8: Color.Yellow,
+        9: Color.Cyan,
+        10: Color.Red,
+        11: Color.Purple,
+    };
+    return rarityColors[rare] || Color.White;
 }
 
 let opacityCycle = 0;
 
+/**
+ * Get the current opacity based on a sine wave cycle.
+ * @returns {number} - The opacity value between 0.5 and 0.65.
+ */
 function getOpacity() {
     const cycleValue = Math.sin(opacityCycle * Math.PI);
     return 0.5 + cycleValue * 0.15;
@@ -122,7 +125,7 @@ Main.DrawItems.hook((orig, self) => {
                     Color_Op_Multiply(rarityColor, opacity),
                     0,
                     getOrigin(20, 20),
-                    4 + item.Widht
+                    4 + item.Width
                 );
             } else {
                 drawTexture(
@@ -131,7 +134,7 @@ Main.DrawItems.hook((orig, self) => {
                     Color_Op_Multiply(rarityColor, opacity),
                     0,
                     getOrigin(20, 20),
-                    1 + item.Widht
+                    1 + item.Width
                 );
             }
 
