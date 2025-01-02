9aee98b3c8e4a7253cb2726dfdf74feb73be7d23
TLEternal <109238149+01Eternal@users.noreply.github.com>
2025-01-02 17:26:43 -0300

    1.5

--- TL/Modified/1.mod/main.js
+++ TL/Modified/1.mod/main.js
@@ -1,19 +1,18 @@
-import { using } from "./ModClasses.js";
+/** @format */
 
-using("Terraria");
-using("Terraria.ID");
-using("Terraria.GameContent");
-using("Microsoft.Xna.Framework");
-using("Microsoft.Xna.Framework.Graphics");
+import { using } from './ModClasses.js';
 
-const Draw =
-    SpriteBatch[
-        "void Draw(Texture2D texture, Rectangle destinationRectangle, Color color)"
-    ];
-const Color_Op_Multiply = Color["Color op_Multiply(Color a, float amount)"];
+using('Terraria');
+using('Terraria.ID');
+using('Terraria.GameContent');
+using('Microsoft.Xna.Framework');
+using('Microsoft.Xna.Framework.Graphics');
+
+const Draw = SpriteBatch['void Draw(Texture2D texture, Rectangle destinationRectangle, Color color)'];
+const Color_Op_Multiply = Color['Color op_Multiply(Color a, float amount)'];
 
 /**
- * Draw a texture with specified parameters.
+ * @summary Draw a texture with specified parameters.
  * @param {Texture2D} texture - The texture to draw.
  * @param {Vector2} position - The position on the screen where the texture will be drawn.
  * @param {Color} color - The color to apply to the texture.
@@ -22,132 +21,103 @@ const Color_Op_Multiply = Color["Color op_Multiply(Color a, float amount)"];
  * @param {number} scale - The scale factor of the texture.
  */
 function drawTexture(texture, position, color, rotation, origin, scale) {
-    Main.spriteBatch[
-        "void Draw(Texture2D texture, Vector2 position, Nullable`1 sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)"
-    ](texture, position, null, color, rotation, origin, scale, null, 0.0);
+	Main.spriteBatch[
+		'void Draw(Texture2D texture, Vector2 position, Nullable`1 sourceRectangle, Color color, float rotation, Vector2 origin, float scale, SpriteEffects effects, float layerDepth)'
+	](texture, position, null, color, rotation, origin, scale, null, 0.0);
 }
 
-import { ModTexture } from "./ModTexture.js";
+function vector2(x, y) {
+	return Vector2.new()['void .ctor(float x, float y)'](x, y);
+}
 
-let SimpleBeam = "";
-let Center = "";
-let SimpleGlow = "";
+class LootBeam {
+	static SIMPLE_BEAM_TEXTURE = null;
+	static SIMPLE_GLOW_BEAM_TEXTURE = null;
+	static colorOpacityCycle = 0;
+	/**
+	 * @summary Get the color corresponding to the item's rarity.
+	 * @param {number} rare - The rarity ID of the item.
+	 * @returns {Color} - The corresponding rarity color.
+	 */
+	static GetRarityByItemRarity(rare) {
+		const RarityIDIndex = {
+			[-11]: Colors.RarityAmber,
+			[-1]: Color.Gray,
+			0: Colors.RarityNormal,
+			1: Colors.RarityBlue,
+			2: Colors.RarityGreen,
+			3: Colors.RarityOrange,
+			4: Colors.RarityRed,
+			5: Colors.RarityPink,
+			6: Colors.RarityPurple,
+			7: Colors.RarityLime,
+			8: Colors.RarityYellow,
+			9: Colors.RarityCyan,
+			10: Colors.RarityRed,
+			11: Color.Purple
+		};
+
+		return RarityIDIndex[rare] || Color.White;
+	}
+
+	/**
+	 * @summary Get the current opacity based on a sine wave cycle
+	 * @returns {number} - The opacity value between 0.5 and 0.65.
+	 */
+	static getColorOpacity() {
+		const cycleValue = Math.sin(LootBeam.colorOpacityCycle * Math.PI);
+
+		return 0.5 + cycleValue * 0.15;
+	}
+
+	static DrawBeam() {
+		Array.from(Main.item).forEach(item => {
+			if (item.active) {
+				let centerX = item.Center.X - Main.screenPosition.X;
+				let centerY = item.Center.Y - Main.screenPosition.Y;
+
+				const ITEM_TEXTURE = TextureAssets.Item[item.type].Value;
+				const RARITY_COLOR = LootBeam.GetRarityByItemRarity(item.rare);
+				const COLOR_OPACITY = LootBeam.getColorOpacity();
+
+				function getOrigin(x, y) {
+					return Vector2.new()['void .ctor(float x, float y)'](x / 2, y / 2);
+				}
+
+				drawTexture(
+					LootBeam.SIMPLE_BEAM_TEXTURE,
+					vector2(centerX - ITEM_TEXTURE.Width / 4 + 8, centerY - 3 * 16),
+					Color_Op_Multiply(RARITY_COLOR, COLOR_OPACITY),
+					0,
+					getOrigin(6, 144),
+					0.75
+				);
+
+				drawTexture(
+					LootBeam.SIMPLE_GLOW_BEAM_TEXTURE,
+					vector2(centerX - ITEM_TEXTURE.Width / 4 + 8, centerY),
+					Color_Op_Multiply(RARITY_COLOR, COLOR_OPACITY),
+					0,
+					getOrigin(150, 150),
+					0.4
+				);
+			}
+		});
+	}
+}
 
 Main.Initialize_AlmostEverything.hook((original, self) => {
-    original(self);
-    SimpleBeam = new ModTexture(`SimpleBeam`).asset.asset.Value;
-    Center = new ModTexture(`Center`).asset.asset.Value;
-    SimpleGlow = new ModTexture(`SimpleGlow`).asset.asset.Value;
+	original(self);
+	LootBeam.SIMPLE_GLOW_BEAM_TEXTURE = tl.texture.load('Textures/SimpleGlow.png');
+	LootBeam.SIMPLE_BEAM_TEXTURE = tl.texture.load('Textures/SimpleBeam.png');
 });
 
-function vector(x, y) {
-    const vector = Vector2.new();
-    vector.X = x;
-    vector.Y = y;
-    return vector;
-}
-
-function rectangle(x, y, width, height) {
-    const rectangle = Rectangle.new();
-    rectangle.X = x;
-    rectangle.Y = y;
-    rectangle.Width = width;
-    rectangle.Height = height;
-    return rectangle;
-}
-
-/**
- * Get the color corresponding to the item's rarity.
- * @param {number} rare - The rarity ID of the item.
- * @returns {Color} - The corresponding rarity color.
- */
-function getRarityColor(rare) {
-    const rarityColors = {
-        [-1]: Color.Gray,
-        0: Color.White,
-        1: Color.Blue,
-        2: Color.Green,
-        3: Color.Orange,
-        4: Color.Red,
-        5: Colors.RarityPink,
-        6: Color.LightPurple,
-        7: Color.Lime,
-        8: Color.Yellow,
-        9: Color.Cyan,
-        10: Color.Red,
-        11: Color.Purple,
-    };
-    return rarityColors[rare] || Color.White;
-}
-
-let opacityCycle = 0;
-
-/**
- * Get the current opacity based on a sine wave cycle.
- * @returns {number} - The opacity value between 0.5 and 0.65.
- */
-function getOpacity() {
-    const cycleValue = Math.sin(opacityCycle * Math.PI);
-    return 0.5 + cycleValue * 0.15;
-}
-
 Player.UpdateEquips.hook((orig, self, i) => {
-    orig(self, i);
-    opacityCycle += 0.05;
+	orig(self, i);
+	LootBeam.colorOpacityCycle += 0.05;
 });
 
 Main.DrawItems.hook((orig, self) => {
-    Array.from(Main.item).forEach(item => {
-        if (item.active) {
-            let centerX = item.Center.X - Main.screenPosition.X;
-            let centerY = item.Center.Y - Main.screenPosition.Y;
-
-            const rarityColor = getRarityColor(item.rare);
-            const opacity = getOpacity();
-
-            function getOrigin(x, y) {
-                return vector(x / 2, y / 2);
-            }
-
-            drawTexture(
-                SimpleBeam,
-                vector(centerX, centerY - 16 * 3),
-                Color_Op_Multiply(rarityColor, opacity),
-                0,
-                getOrigin(6, 144),
-                0.75
-            );
-
-            if (item.damage > 0) {
-                drawTexture(
-                    Center,
-                    vector(centerX, centerY),
-                    Color_Op_Multiply(rarityColor, opacity),
-                    0,
-                    getOrigin(20, 20),
-                    4 + item.Width
-                );
-            } else {
-                drawTexture(
-                    Center,
-                    vector(centerX, centerY),
-                    Color_Op_Multiply(rarityColor, opacity),
-                    0,
-                    getOrigin(20, 20),
-                    1 + item.Width
-                );
-            }
-
-            drawTexture(
-                SimpleGlow,
-                vector(centerX, centerY),
-                Color_Op_Multiply(rarityColor, opacity),
-                0,
-                getOrigin(150, 150),
-                0.35
-            );
-        }
-    });
-
-    orig(self);
+	LootBeam.DrawBeam();
+	orig(self);
 });
